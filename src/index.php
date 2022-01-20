fb68dcb866dd62c8fbb434bc3530765fe2ea2eef
AngelD13 <mirgorodfor@gmail.com>
2022-01-21 00:30:01 +0200

    database connection

--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <?php
 
+$pdo = new \PDO("mysql:host=mysql; dbname=php_app", "root", "mysql");
+
+print_r($pdo->query("SELECT * FROM users")->fetchAll());
+
+echo "<br>_______________</br>";
+
 phpinfo();
 
 ?>
